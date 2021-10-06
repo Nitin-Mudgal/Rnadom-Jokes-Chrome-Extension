deaa4cad23df4295573514500e9a9b636c84bf32
Nitin-Mudgal <57403335+Nitin-Mudgal@users.noreply.github.com>
2021-10-06 11:21:09 +0530

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,21 @@
+/*fetch('https://icanhazdadjoke.com/slack')
+.then(data=>data.json())
+.then(jokeData=>{
+    const jokeText=jokeData.attachments[0].text;
+    const jokeElement=document.getElementById('jokeElement');
+
+    jokeElement.innerHTML=jokeText;
+
+
+
+}
+    )*/
+
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+
+        jokeElement.innerHTML = jokeText;
+    })
